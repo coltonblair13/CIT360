bd0e367e225e4a249fbd438224882ba39292877e
Colton Blair <coltonblair13@gmail.com>
2015-02-07 13:06:05 -0700

    First commit

---
+++ temp.java
@@ -0,0 +1,45 @@
+import java.io.Serializable;
+
+
+--Every JavaBean has to be serializable
+
+
+--Creating a constructor
+
+public class Temperature implements Serializable  {
+	public Temperature() {
+		temp = 0.0;
+}
+
+
+--Must Create getters and setter for Javabeans. A temperature value must be returned or set. Meaning a value is either being returned or passed in.
+
+--getter called getTemp
+
+	public double getTemp() {
+		return temp;
+ }
+
+
+--setter called setTemp which is paramater T
+
+	public void setTemp (double t) {
+ 		temp = t;
+ }
+
+--Write methods that are necessary for the class
+
+--cToF method converts celsius to Farenheit
+
+	public void cToF() {
+		temp = temp * (9.0/5.0) + 32.0;
+
+ }
+
+--fToC method converts farenheit to celsius
+
+	public void fToC() {
+		temp = (temp - 32.0) * (5.0/9.0);
+ }
+
+
---
+++ tempinfo.java
@@ -0,0 +1,44 @@
+--This gains access to the java beans classes
+
+import java.beans.*;
+import jav.lan.reflect.Method;
+
+public class TempInfo extends SimpleBeanInfo {
+	public PropertyDescriptor() getPropertyDescriptors() {
+		try {
+			PropertyDescriptor temp = new
+				PropertyDescriptor("temp", Temperature.class);
+			PropertyDescriptor pd[] = (temp);
+			return.pd;
+		}
+		catch (Exception e) {
+			System.out.println("Exception thrown.");
+		}
+		return null;
+	}
+
+	public MethodDescriptor[] getMethodDescriptors() {
+		try {
+			Class cl = Temperature.class;
+			Class args[] = ( );
+			Method cToF = cl.getMethod("cToF", args);
+			MethodDescriptor cToFDesc = new
+				MethodDescriptor(cToF);
+			cToFDesc.setShortDescription("Convert Celsius to Fahrenheit");
+
+			Method fToC = cl.getMethod("fToC", args);
+			MethodDescriptor fToCDesc = new
+				MethodDescriptor(fToC);
+			cToFDesc.setShortDescription("Convert Fahrenheit to Celsius");
+
+			MethodDescriptor[] md = {cToFDesc, fToCDesc);
+			return md;
+			}
+			catch (Exception e) {
+				System.out.prinln("Exception thrown.");
+			}
+			return null;
+
+	}
+
+}
